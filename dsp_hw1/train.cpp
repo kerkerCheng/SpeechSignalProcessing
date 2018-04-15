df837936c8d9e6cb8d92079ed69e16a7ef02ff72
kerkerCheng <c3035281@gmail.com>
2018-04-16 02:24:36 +0800

    Finish basic prob.I & basic prob.II

--- dsp_hw1/train.cpp
+++ dsp_hw1/train.cpp
@@ -24,13 +24,25 @@
 
 using namespace std;
 
-void alpha_calculate();
+void alpha_calculate(double **alpha, vector<int>& seq, HMM &hmm);
+void beta_calcaulate(double **beta, vector<int>& seq, HMM &hmm);
+void epsilon_calculate(double ***ep, double **alpha, double **beta, double **ijmap, vector<int>& seq, HMM* hmm);
+void gamma_calculate(double **gamma, double **alpha, double **beta, vector<int>& seq);
+void update_a(double **a_bar, double ***ep, double **ga, vector<int>& seq, HMM *hmm);
+void update_pi(double **ga, HMM *hmm);
+void update_observation(double **ga, HMM *hmm, vector<int>& seq);
+double viterbi(double** del, HMM* hmm, vector<int>& seq);
+double array_sum(double **arr, int row, int col);
+
 
 int main()
 {
 	HMM hmm_initial;
 	HMM hmm[MD_NUM];
 	loadHMM(&hmm_initial, "model_init.txt");
+
+	for(int i=0; i<5; i++)
+		hmm[i] = hmm_initial;
 
 	ifstream seq_files[MD_NUM];
 	for(int i=0; i<MD_NUM; i++)
@@ -52,10 +64,10 @@ int main()
 		{
 			istringstream iss(line);
 			char ch;
-			vector<char> one_line;
+			vector<int> one_line;
 			while(iss >> ch)
 			{
-				int ch_int;
+				int ch_int=0;
 				switch(ch)
 				{
 					case 'A':
@@ -83,7 +95,47 @@ int main()
 		}
 	}
 
+	/* Initialize */
+	double **alpha = new double*[STATE_NUM];
+	double **beta = new double*[STATE_NUM];
+	double **gamma = new double*[STATE_NUM];
+	double **ijmap = new double*[STATE_NUM];
+	double **a_bar = new double*[STATE_NUM];
+	double ***epsilon = new double**[SEQ_NUM-1];
+
+	for(int i=0; i<(SEQ_NUM-1); i++)
+	{
+		epsilon[i] = new double*[STATE_NUM];
+		for(int j=0; j<STATE_NUM; j++)
+			epsilon[i][j] = new double[STATE_NUM];
+	}
+
+	for(int i=0; i<STATE_NUM; i++)
+	{
+		alpha[i] = new double[SEQ_NUM];
+		beta[i] = new double[SEQ_NUM];
+		gamma[i] = new double[SEQ_NUM];
+		ijmap[i] = new double[STATE_NUM];
+		a_bar[i] = new double[STATE_NUM];
+	}
+
+	/* Training */
+	for(int i=0; i<5; i++)
+	{
+		for(vector< vector<int> >::iterator it1 = seq[i].begin(); it1 != seq[i].end(); ++it1)
+		{
+			alpha_calculate(alpha, *it1, hmm[i]);
+			beta_calcaulate(beta, *it1, hmm[i]);
+			epsilon_calculate(epsilon, alpha, beta, ijmap, *it1, &hmm[i]);
+			gamma_calculate(gamma, alpha, beta, *it1);
+
+			update_a(a_bar, epsilon, gamma, *it1, &hmm[i]);
+			update_pi(gamma, &hmm[i]);
+			update_observation(gamma, &hmm[i], *it1);
+		}
+	}
 	
+
 
 	
 	// FILE *f;
@@ -136,19 +188,19 @@ void beta_calcaulate(double **beta, vector<int>& seq, HMM &hmm)
 	}
 }
 
-void epsilon_calculate(double ***ep, double **alpha, double **beta, double **ijmap, vector<int>& seq, HMM &hmm)
+void epsilon_calculate(double ***ep, double **alpha, double **beta, double **ijmap, vector<int>& seq, HMM* hmm)
 {
 	int seq_len = seq.size();
 
-	for(int t=0; t<seq_len; t++)
+	for(int t=0; t<(seq_len-1); t++)
 	{
 		for(int i=0; i<STATE_NUM; i++)
 		{
 			for(int j=0; j<STATE_NUM; j++)
-				ijmap[i][j] = alpha[i][t] * hmm.transition[i][j] * hmm.observation[j][seq[t+1]] * beta[j][t+1];
+				ijmap[i][j] = alpha[i][t] * hmm->transition[i][j] * hmm->observation[j][seq[t+1]] * beta[j][t+1];
 		}
 
-		double deno = array_sum(ijmap);
+		double deno = array_sum(ijmap, STATE_NUM, STATE_NUM);
 
 		for(int i=0; i<STATE_NUM; i++)
 		{
@@ -175,9 +227,9 @@ void gamma_calculate(double **gamma, double **alpha, double **beta, vector<int>&
 	}
 }
 
-void **a_bar_calculate(double **a_bar, double **ep, double **ga, vector<int>& seq)
+void update_a(double **a_bar, double ***ep, double **ga, vector<int>& seq, HMM *hmm)
 {
-	int seq_len = seq.size();
+	int sample_size = seq.size();
 
 	for(int i=0; i<STATE_NUM; i++)
 	{
@@ -194,9 +246,96 @@ void **a_bar_calculate(double **a_bar, double **ep, double **ga, vector<int>& se
 			a_bar[i][j] = nume/deno;
 		}
 	}
+
+	for(int i=0; i<STATE_NUM; i++)
+	{
+		for(int j=0; j<STATE_NUM; j++)
+			hmm->transition[i][j] = a_bar[i][j];
+	}
 }
 
-double array_sum(int row, int col, double **arr)
+void update_pi(double **ga, HMM *hmm)
+{
+	for(int i=0; i<STATE_NUM; i++)
+		hmm->initial[i] = ga[i][0];
+}
+
+void update_observation(double **ga, HMM *hmm, vector<int>& seq)
+{
+	int seq_len = seq.size();
+	for(int i=0; i<STATE_NUM; i++)
+	{
+		double deno = 0;
+		double nume[STATE_NUM] = {0};
+		for(int t=0; t<seq_len; t++)
+		{
+			deno += ga[i][t];
+			switch(seq[t])
+			{
+				case 0:
+					nume[0] += ga[i][t];
+					break;
+				case 1:
+					nume[1] += ga[i][t];
+					break;
+				case 2:
+					nume[2] += ga[i][t];
+					break;
+				case 3:
+					nume[3] += ga[i][t];
+					break;
+				case 4:
+					nume[4] += ga[i][t];
+					break;
+				case 5:
+					nume[5] += ga[i][t];
+					break;
+			}
+		}
+
+		for(int j=0; j<MAX_OBSERV; j++)
+		{
+			hmm->observation[i][j] = nume[j]/deno;
+		}
+	}
+}
+
+double viterbi(double** del, HMM* hmm, vector<int>& seq)
+{
+	int seq_len = seq.size();
+	for(int i=0; i<STATE_NUM; i++)
+		del[i][0] = hmm->initial[i] * hmm->observation[i][seq[0]];
+
+	for(int t=1; t<seq_len; t++)
+	{
+		for(int j=0; j<STATE_NUM; j++)
+		{
+			double max = 0;
+			int max_state = 0;
+			for(int i=0; i<STATE_NUM; i++)
+			{
+				double t = del[i][t-1] * hmm->transition[i][j];
+				if(t > max)
+				{
+					max = t;
+					max_state = i;
+				}
+			}
+			del[j][t] = max * hmm->observation[j][seq[t]];
+		}
+	}
+
+	double p_star = 0;
+	for(int i=0; i<STATE_NUM; i++)
+	{
+		if(del[i][seq_len-1] > p_star)
+			p_star = del[i][seq_len-1];
+	}
+	
+	return p_star;
+}
+
+double array_sum(double **arr, int row, int col)
 {
 	double sum = 0;
 	for(int i=0; i<row; i++)
