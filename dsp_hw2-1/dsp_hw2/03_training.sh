1b01e973dce3dbd4c71c82b51883b1161704e256
kerkerCheng <c3035281@gmail.com>
2018-05-13 12:16:34 +0800

    acc = 92.92

--- dsp_hw2-1/dsp_hw2/03_training.sh
+++ dsp_hw2-1/dsp_hw2/03_training.sh
@@ -49,10 +49,38 @@ done
 #################################################
 # increase mixture
 echo "step 04 [HHEd]: split gaussian mixture..."
+HHEd -T 2 -H $macro -H $model -M $mmf_dir lib/mix2_10.hed $model_list
+
+# re-adjust mean, var
+echo "step 05 [HErest]: adjust mean, var..."
+for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30;
+do
+	echo "iteration $i"
+	HERest -C $config -I $label \
+		-t 250.0 150.0 1000.0 -S $data_list \
+		-H $macro -H $model -M $mmf_dir $model_list
+done
+
+# increase mixture
+echo "step 06 [HHEd]: split gaussian mixture..."
 HHEd -T 2 -H $macro -H $model -M $mmf_dir lib/mixAdd2_10.hed $model_list
 
 # re-adjust mean, var
-echo "step 05 [HERest]: adjust mean, var..."
+echo "step 07 [HErest]: adjust mean, var..."
+for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30;
+do
+	echo "iteration $i"
+	HERest -C $config -I $label \
+		-t 250.0 150.0 1000.0 -S $data_list \
+		-H $macro -H $model -M $mmf_dir $model_list
+done
+
+# increase mixture
+echo "step 08 [HHEd]: split gaussian mixture..."
+HHEd -T 2 -H $macro -H $model -M $mmf_dir lib/mixAdd2_10.hed $model_list
+
+# re-adjust mean, var
+echo "step 09 [HERest]: adjust mean, var..."
 for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60;
 do
 	HERest -C $config -I $label \
